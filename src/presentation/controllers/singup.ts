ed235f5a0b1bcfb199d43cb427d5f6b46686fdd9
0x2moon <0x2moon@protonmail.com>
2025-08-13 10:32:54 -0400

    refactor: ensure SingUpController implement corret interfaces

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,5 +1,6 @@
+import type {httpResponse, httpRequest} from '../protocols/http'
 export class SignUpController {
-    handle (httpRequest:any): any {
+    handle (httpRequest:httpRequest): httpResponse {
        if(!httpRequest.body.name) {
          return {
             statusCode:400,
@@ -13,5 +14,9 @@ export class SignUpController {
             body: new Error('Missing param: email')
         }
        }
+        return {
+          statusCode: 200,
+          body: { message: 'Success' }
+        }
     }
 }
